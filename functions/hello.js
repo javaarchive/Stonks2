b1878d5a66c09871229b8472daf7513b13449fdc
Raymond <raymond.peng07@gmail.com>
2022-01-27 18:43:53 +0000

    test code and new file

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello!" }),
+      body: JSON.stringify({ message: "Hello! Deta key length " + (process.env.DETA_PROJECT || "").length }),
     };
   }
