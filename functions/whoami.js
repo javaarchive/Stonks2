ee9b0d7117d4a6b534d8422f603d8405e6fae8e3
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 17:03:59 -0800

    more testing, again do not use

--- functions/whoami.js
+++ functions/whoami.js
@@ -5,7 +5,7 @@ exports.handler = async function (event, context) {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            identity, user
+            identity, user, context.clientContext
         })
     }
 };
