6a1a3b89c36f9269f9ee1a6c7aeca2a3d869e0db
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 16:49:43 -0800

    fetch stocks api with cache support

--- functions/fetch_current_stocks.js
+++ functions/fetch_current_stocks.js
@@ -1,8 +1,12 @@
-
+const db = require("../shared/db");
 
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello!" }),
+      body: JSON.stringify(await db.stocks.fetch()),
+      headers:{
+        "cache-control": "public, max-age=60",
+        "age": "10"
+      }
     };
   }
