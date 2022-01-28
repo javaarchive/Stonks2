77dcd542253d9379969b4bc7a186e347a6a4ca2b
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 16:51:22 -0800

    fix api to remove extra info

--- functions/fetch_current_stocks.js
+++ functions/fetch_current_stocks.js
@@ -3,7 +3,7 @@ const db = require("../shared/db");
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify(await db.stocks.fetch()),
+      body: JSON.stringify((await db.stocks.fetch())["items"]),
       headers:{
         "cache-control": "public, max-age=60",
         "age": "10"
