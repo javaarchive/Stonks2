28b1cd00ad93b0250c3a05d5fa52aff419fd42b9
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 19:06:05 -0800

    user system

--- shared/config.js
+++ shared/config.js
@@ -15,5 +15,8 @@ module.exports = {
         "AMC",
         "INTC"
     ],
-    fetchIntervalLimit: 1000 * 60 * 2 - 5000 // slightly less than 2 minutes
+    fetchIntervalLimit: 1000 * 60 * 2 - 5000, // slightly less than 2 minutes
+    defaultUserData: {
+        money: 1000.00
+    }
 }
--- shared/users.js
+++ shared/users.js
@@ -1,12 +1,51 @@
-module.exports = {
+const db = require("./db");
+const config = require("./config");
+const { defaultUserData } = require("./config");
+
+let self = {
     processContext: (context) => {
         const claims = context.clientContext && context.clientContext.user;
         if (!claims) {
             return null;
         }
-        return {
+        /*return {
             user: claims.sub,
             raw: claims
+        }*/
+        return {
+            email: claims.email,
+            from: claims.app_metadata.provider,
+            avatar_url: claims.user_metadata.avatar_url,
+            full_name: claims.full_name,
+            id: claims.sub,
+            key: claims.sub
         }
+    },
+    putUser: (user) => {
+        return db.users.put(user);
+    },
+    getUser: (id) => {
+        return db.users.get(id);   
+    },getUserByEmail: async (email) => {
+        let resp = await db.users.fetch({email});
+        if(!resp || resp.items.length <= 0){
+            return null;
+        }
+        return resp.items[0];   
+    },userify: async (context) => {
+        let userBasic = self.processContext(id);
+        let userFetched = await self.getUser(userBasic.key);
+        if(!userFetched){
+            // create an account
+            await self.putUser({...config.defaultUserData,...userBasic});
+            return userBasic;
+        }else{
+            let userUpdated = {...config.defaultUserData,...userFetched,...userBasic};
+            await self.putUser(userUpdated);
+            return userUpdated;
+        }
+        
     }
 }
+
+module.exports = self;
