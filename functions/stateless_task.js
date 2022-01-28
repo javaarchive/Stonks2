a93ec7f9c792b3ce556a5af934e80f5c17ab051f
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 16:23:12 -0800

    deploy stock ticker updater

--- functions/stateless_task.js
+++ functions/stateless_task.js
@@ -1,6 +1,10 @@
 const db = require("../shared/db");
 const config = require("../shared/config");
 
+const yahooFinance = require('yahoo-finance2').default;
+
+let skipCheck = false;
+
 exports.handler = async function (event, context) {
     try{
         let lastState = (await db.state.get("task_state")) || {};
@@ -8,16 +12,45 @@ exports.handler = async function (event, context) {
         if(lastState.lastStock){
             index = (config.tickers.indexOf(lastState.lastStock) + 1);
         }
+        if(lastState.lastFetch > 0 && !skipCheck){
+            if((Date.now() - lastState.lastFetch) < config.fetchIntervalLimit){
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ ok: true }),
+                    message: "Fetch interval limit reached"
+                };
+            }
+        }
         index = index % config.tickers.length; // Cycle if needed. 
 
         // Fetch data
 
-        // simulation
+        let tickerToFetch = config.tickers[index];
+
+        let data = await yahooFinance.quote(tickerToFetch);
+
+        await db.stocks.put({
+            key: tickerToFetch,
+            price: data.regularMarketPrice,
+            date: Date.now(),
+            currency: data.currency,
+            name: data.displayName,
+            shortName: data.shortName,
+            symbol: data.symbol,
+            exchange: data.exchange,
+            longName: data.longName,
+            quoteSource: data.quoteSourceName,
+            market: data.market
+        });
+
+        console.log(tickerToFetch,data);
 
         await db.state.put({
             key: "task_state",
-            lastStock: config.tickers[index]
-        })
+            lastStock: tickerToFetch,
+            lastFetch: Date.now()
+        });
+
     }catch(ex){
         return {
             statusCode: 500,
@@ -28,4 +61,12 @@ exports.handler = async function (event, context) {
       statusCode: 200,
       body: JSON.stringify({ ok: true })
     };
-  }
+}
+
+// Allow running directly
+if(require.main === module){
+    skipCheck = true;
+    exports.handler({},{}).then(result => {
+        console.log("Simulated Response",result);
+    });
+}
--- shared/config.js
+++ shared/config.js
@@ -1,6 +1,19 @@
 module.exports = {
     tickers: [
         "AAPL",
-        "MSFT"
-    ]
+        "MSFT",
+        "AMZN",
+        "GOOG",
+        "FB",
+        "TWTR",
+        "NFLX",
+        "TSLA",
+        "GME",
+        "NVDA",
+        "IBM",
+        "AMD",
+        "AMC",
+        "INTC"
+    ],
+    fetchIntervalLimit: 1000 * 60 * 5 - 5000 // slightly less than 5 minutes
 }
