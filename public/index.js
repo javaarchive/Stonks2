3b6962401b55add0570f473b0df3eb83bc783314
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 17:29:27 -0800

    vuetify loader

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
 // Identity Code
 
+let identity = {
+    genAuthHeader: () => {
+        if(!netlifyIdentity.currentUser()){
+            return {};
+        }
+        return {
+            "Authorization": `Bearer ${netlifyIdentity.currentUser().jwt()}`
+        }
+    }
+}
+
 // Main Code
 
 docReady(() => {
@@ -8,7 +19,29 @@ docReady(() => {
     let app = new Vue({
     el: '#root',
         data: {
-            
+            stocks: [
+
+            ]
+        },methods: {
+
+        }
+    });
+
+    fetch("/.netlify/functions/fetch_current_stocks", {
+        method: "GET",
+        headers: {
+            source: "initial-fetch",
+            ...identity.genAuthHeader()
+        }
+    }).then(resp => {
+        if(resp.status == 200){
+            resp.json().then(data => {
+                app.stocks = data.items; // update!
+            });
+        }else{
+            resp.text().then(text => {
+                alert("Server gave an error for you: " + text);
+            })
         }
     })
     
