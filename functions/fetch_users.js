69f9fc0b2badfdf3773a4dad26811d518a1054f0
Raymomyar <awesomecatstudio@gmail.com>
2022-01-27 19:38:42 -0800

    fix typo

--- functions/fetch_users.js
+++ functions/fetch_users.js
@@ -3,7 +3,7 @@ const db = require("../shared/db");
 exports.handler = async function (event, context) {
   let items = (await db.users.fetch())["items"];
 
-  let filteredItems = items.filter(user => {
+  let filteredItems = items.map(user => {
     delete user.email;
     delete user.key;
   });
